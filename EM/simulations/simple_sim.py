9267a066bb4f2cf7ea2b1e5fb775d67669ae6927
christa caggiano <christacaggiano@gmail.com>
2021-04-14 17:26:27 -0700

    prettier code 2

--- EM/simulations/simple_sim.py
+++ EM/simulations/simple_sim.py
@@ -1,22 +1,22 @@
-
-import numpy as np 
-import pandas as pd 
-import random 
+import numpy as np
+import pandas as pd
+import random
 import pickle as pkl
 import bottleneck as bn
-import sys 
+import sys
 import math
 import os
-from collections import defaultdict 
+from collections import defaultdict
 
 ## Functions
+
 
 def generate_alpha(pkl_file):
 
     """
-    reads in pickle file dictating tissue proportions 
+    reads in pickle file dictating tissue proportions
     :param str pickle file of the tissue proportions (people X tissues)
-    :return: values loaded as a numpy array 
+    :return: values loaded as a numpy array
 
     """
 
@@ -24,64 +24,73 @@ def generate_alpha(pkl_file):
     return t.T
 
 
-def generate_gamma(i, j): 
+def generate_gamma(i, j):
     """
-    randomly generates a matrix of methylation values between 0 and 1 
-    :param int tissue: number of tissues 
+    randomly generates a matrix of methylation values between 0 and 1
+    :param int tissue: number of tissues
     :param int cpg: number of cpgs
-    :return: cpg x tissue matrix of random methylation values 
+    :return: cpg x tissue matrix of random methylation values
     """
-    
+
     gamma = np.zeros((i, j))
-    
-    for n in range (j): 
-        gamma[:, n] = np.random.uniform(0, 1, size=i)  # draws from a uniform distribution 
-    
+
+    for n in range(j):
+        gamma[:, n] = np.random.uniform(
+            0, 1, size=i
+        )  # draws from a uniform distribution
+
     return gamma
 
 
-def generate_beta(alpha, gamma, x_depths): 
+def generate_beta(alpha, gamma, x_depths):
     """
-    generates the cfDNA reads based on the generated tissue proportions, the true 
-    methylation values in the reference and the depths 
-    :param array alpha: tissue props 
-    :param array gamma: methylation values for the reference 
-    :param array x_depths: simulated read depths for each CpG in each individual in cfDNA input 
+    generates the cfDNA reads based on the generated tissue proportions, the true
+    methylation values in the reference and the depths
+    :param array alpha: tissue props
+    :param array gamma: methylation values for the reference
+    :param array x_depths: simulated read depths for each CpG in each individual in cfDNA input
     :return: methylation reads for the cfDNA input (number of sites X number of individuals)
 
     """
-    
+
     total_indiv = alpha.shape[0]
     i, total_cpg = gamma.shape
-    
+
     beta = np.zeros((total_indiv, total_cpg))
-    
-    for n in range(total_indiv): 
-        for j in range(total_cpg): 
-           
-            depth = x_depths[n, j]  # depth at a paticular cpg and person 
+
+    for n in range(total_indiv):
+        for j in range(total_cpg):
+
+            depth = x_depths[n, j]  # depth at a paticular cpg and person
             gamma_cpg = gamma[:, j]  # "true" methylation value in the reference
 
-            mix = np.random.choice(i, depth, replace=True, p=alpha[n, ])  # assign reads based on the tissue proportions for that individual 
+            mix = np.random.choice(
+                i,
+                depth,
+                replace=True,
+                p=alpha[
+                    n,
+                ],
+            )  # assign reads based on the tissue proportions for that individual
             probability = gamma_cpg[mix]
-            
-            beta[n, j] = np.sum(np.random.binomial(1, probability, size=depth))  # the beta is the sum of all the individual reads coming from the tissues contributing to that cpg in that individual 
-            
+
+            beta[n, j] = np.sum(
+                np.random.binomial(1, probability, size=depth)
+            )  # the beta is the sum of all the individual reads coming from the tissues contributing to that cpg in that individual
+
     return beta
-            
 
 
-def generate_counts(count, probability): 
+def generate_counts(count, probability):
     """
-    generate the methylation read counts for the reference data 
-    :param array count: read depths 
-    :param array probability: probability of each cpg being methylated for each tissue 
+    generate the methylation read counts for the reference data
+    :param array count: read depths
+    :param array probability: probability of each cpg being methylated for each tissue
     :return: array of methylated reads
 
     """
-    
-    return np.random.binomial(count, probability, size=probability.shape)
 
+    return np.random.binomial(count, probability, size=probability.shape)
 
 
 def generate_depths(depth, input_shape):
@@ -89,241 +98,257 @@ def generate_depths(depth, input_shape):
     :param int depth: read depth
     :param tuple input_shape: number of tissues X number of CpGs
     :return: array of ints where each number represents the number of total reads in a tissue at a cpg
-    """ 
-   
+    """
+
     return np.random.poisson(depth, input_shape)
 
 
-def generate_em_replicate(i, j, n, depth, gamma_depth, pkl_file): 
-    
+def generate_em_replicate(i, j, n, depth, gamma_depth, pkl_file):
+
     # let alpha be a simple increasing vector of proportions
-    alpha_int = np.array(list(range(1, i+1)))
-    alpha_int = (alpha_int/alpha_int.sum()).reshape(1, len(alpha_int))
-    alpha_int = np.repeat(alpha_int, n, axis=0)  # repeat for the number of individuals 
+    alpha_int = np.array(list(range(1, i + 1)))
+    alpha_int = (alpha_int / alpha_int.sum()).reshape(1, len(alpha_int))
+    alpha_int = np.repeat(alpha_int, n, axis=0)  # repeat for the number of individuals
 
     gamma_int = generate_gamma(i, j)
-    
+
     Y_int_depths = generate_depths(gamma_depth, (i, j))
     Y_int = generate_counts(Y_int_depths, gamma_int)
-    
-    X_int_depths = generate_depths(depth, (n, j))
-    X_int = generate_beta(alpha_int, gamma_int, X_int_depths) 
 
-    # set one unknown 
-    Y_int_depths[-1] = 0   
-    Y_int[-1] = 0  
+    X_int_depths = generate_depths(depth, (n, j))
+    X_int = generate_beta(alpha_int, gamma_int, X_int_depths)
+
+    # set one unknown
+    Y_int_depths[-1] = 0
+    Y_int[-1] = 0
 
     return Y_int_depths, Y_int, X_int_depths, X_int, alpha_int, gamma_int
 
 
 def add_pseduocounts(value, array, meth, meth_depths):
-    """ finds values of gamma where logll cannot be computed, adds pseudo-counts to make 
-    computation possible 
+    """finds values of gamma where logll cannot be computed, adds pseudo-counts to make
+    computation possible
 
     :param int value: checks for a value that will prevent computation; either 0 or 1
-    :param array array: gamma array to check for inproper value 
-    :param array meth: np array of methylation counts 
-    :param array meth_depths: np array of total number of reads (meth counts + unmethylated counts) 
+    :param array array: gamma array to check for inproper value
+    :param array meth: np array of methylation counts
+    :param array meth_depths: np array of total number of reads (meth counts + unmethylated counts)
 
     """
 
-    axis0, axis1 = np.where(array==value)  # find indices where value isn't able to be computed 
+    axis0, axis1 = np.where(
+        array == value
+    )  # find indices where value isn't able to be computed
 
-    for i, j in zip(axis0, axis1): 
-        meth[i, j] += 1  # add one read to methylated counts 
+    for i, j in zip(axis0, axis1):
+        meth[i, j] += 1  # add one read to methylated counts
         meth_depths[i, j] += 2  # adds two reads to total counts
 
 
-def check_gamma(array): 
-    """ checks for values of gamma where log likelihood cannot be computed, returns 
-    true if can be computed 
-    
-    :param array array: np array to check 
-    :return: whether there is a 0 or 1 in the array 
-    """ 
+def check_gamma(array):
+    """checks for values of gamma where log likelihood cannot be computed, returns
+    true if can be computed
 
-    return (0 in array) or (1 in array) 
+    :param array array: np array to check
+    :return: whether there is a 0 or 1 in the array
+    """
+
+    return (0 in array) or (1 in array)
 
 
 ########  expectation-maximization algorithm  ########
 
 
 def expectation(gamma, alpha):
-    """ calculates the components needed for loglikelihood for each iteration of gamma and alpha 
+    """calculates the components needed for loglikelihood for each iteration of gamma and alpha
 
-    gamma: np matrix of the estimated 'true' methylation proportions 
-    alpha: np matrix of estimated mixing proportions """
-    
+    gamma: np matrix of the estimated 'true' methylation proportions
+    alpha: np matrix of estimated mixing proportions"""
 
     individuals, tissues = alpha.shape
     sites = gamma.shape[1]
 
-    # pre-defines probability matrices  
+    # pre-defines probability matrices
     p0 = np.zeros((tissues, sites, individuals))
     p1 = np.zeros((tissues, sites, individuals))
 
     for n in range(individuals):
         for j in range(sites):
-            p0_j = (1-gamma[:, j])*alpha[n, : ]
-            p0_j = p0_j/(bn.nansum(p0_j))
-            
-            p1_j = (gamma[:, j])*alpha[n, : ]
-            p1_j = p1_j/(bn.nansum(p1_j))
-            
-            p0[:, j, n] = p0_j 
-            p1[:, j, n] = p1_j 
-            
+            p0_j = (1 - gamma[:, j]) * alpha[n, :]
+            p0_j = p0_j / (bn.nansum(p0_j))
+
+            p1_j = (gamma[:, j]) * alpha[n, :]
+            p1_j = p1_j / (bn.nansum(p1_j))
+
+            p0[:, j, n] = p0_j
+            p1[:, j, n] = p1_j
+
     return p0, p1
 
 
 def log_likelihood(p0, p1, x_depths, x, y_depths, y, gamma, alpha):
-    """ calculates the log likelihood P(X, Z, Y | alpha, gamma)
+    """calculates the log likelihood P(X, Z, Y | alpha, gamma)
 
-    p0: probability that read is methylated 
-    p1: probability read is unmethylated 
+    p0: probability that read is methylated
+    p1: probability read is unmethylated
     x_depths: input read depths
     x: input methylated reads
     y_depths: reference matrix read depths
     y: reference methylated counts
-    gamma: estimated true methylation proportions 
-    alpha: estimated mixing proportions """
+    gamma: estimated true methylation proportions
+    alpha: estimated mixing proportions"""
 
     tissues, sites, individuals = p0.shape[0], p0.shape[1], p0.shape[2]
 
-    # for easier calculation, split the ll into two terms and calculate iteratively 
+    # for easier calculation, split the ll into two terms and calculate iteratively
     term1 = 0
-    term2 = 0 
-    ll = 0 
+    term2 = 0
+    ll = 0
 
-    for n in range(individuals): 
-        for j in range(sites): 
+    for n in range(individuals):
+        for j in range(sites):
             for i in range(tissues):
 
-                # calculates the q-function; see writeup 
-                term1 += np.nan_to_num(y[i, j] + (p1[i, j, n]*x[n, j]))*np.log(gamma[i,j]) + (y_depths[i, j] - y[i, j] + p0[i, j, n]*(x_depths[n, j] - x[n, j]))*np.log(1-gamma[i,j])
-                term2 += np.nan_to_num(((p1[i, j, n]*x[n, j]) + ((x_depths[n, j] - x[n, j])*p0[i, j, n])) * np.log(alpha[n, i]))
-                
+                # calculates the q-function; see writeup
+                term1 += np.nan_to_num(y[i, j] + (p1[i, j, n] * x[n, j])) * np.log(
+                    gamma[i, j]
+                ) + (
+                    y_depths[i, j] - y[i, j] + p0[i, j, n] * (x_depths[n, j] - x[n, j])
+                ) * np.log(
+                    1 - gamma[i, j]
+                )
+                term2 += np.nan_to_num(
+                    (
+                        (p1[i, j, n] * x[n, j])
+                        + ((x_depths[n, j] - x[n, j]) * p0[i, j, n])
+                    )
+                    * np.log(alpha[n, i])
+                )
+
                 ll += term1 + term2
-    return ll 
+    return ll
 
 
-def maximization(p0, p1, x, x_depths, y, y_depths): 
+def maximization(p0, p1, x, x_depths, y, y_depths):
 
-    """ maximizes log-likelihood, calculated in the expectation step
-    calculates new alpha and gamma given these new parameters 
+    """maximizes log-likelihood, calculated in the expectation step
+    calculates new alpha and gamma given these new parameters
 
-    p0: probability that read is methylated 
-    p1: probability read is unmethylated 
+    p0: probability that read is methylated
+    p1: probability read is unmethylated
     x_depths: input read depths
     x: input methylated reads
     y_depths: reference matrix read depths
-    y: reference methylated counts """
-
+    y: reference methylated counts"""
 
     individuals = p0.shape[2]
 
-    # initialize vector 
+    # initialize vector
     ones_vector = np.ones(shape=(y.shape[0]))
     new_alpha = np.zeros((x.shape[0], y.shape[0]))
 
-    # in case of overflow or error, transform nans to 0 and inf to large float  
+    # in case of overflow or error, transform nans to 0 and inf to large float
     p0 = np.nan_to_num(p0)
     p1 = np.nan_to_num(p1)
     x = np.nan_to_num(x)
     x_depths = np.nan_to_num(x_depths)
 
-    # break up calculation into two terms 
-    term0 = 0 
-    term1 = 0 
+    # break up calculation into two terms
+    term0 = 0
+    term1 = 0
 
-    for n in range(individuals): 
-       
-        new_alpha[n, :] = np.dot(p1[:, :, n], x[n,:]) + np.matmul(p0[:, :, n], (x_depths[n, :]-x[n, :]))
-    
-        term1 +=  p1[:, :, n]*(np.outer(ones_vector, x[n,:]))
-        term0 +=  p0[:, :, n]*(np.outer(ones_vector, x_depths[n,:]-x[n,:]))
-    
-    gamma = (term1 + y) / (term0 + term1 + y_depths)  # calculate new gamma 
+    for n in range(individuals):
+
+        new_alpha[n, :] = np.dot(p1[:, :, n], x[n, :]) + np.matmul(
+            p0[:, :, n], (x_depths[n, :] - x[n, :])
+        )
+
+        term1 += p1[:, :, n] * (np.outer(ones_vector, x[n, :]))
+        term0 += p0[:, :, n] * (np.outer(ones_vector, x_depths[n, :] - x[n, :]))
+
+    gamma = (term1 + y) / (term0 + term1 + y_depths)  # calculate new gamma
 
     # check if gamma goes out of bounds, if so add psuedocounts to misbehaving y values
-    if check_gamma(gamma): 
+    if check_gamma(gamma):
         add_pseduocounts(1, gamma, y, y_depths)
         add_pseduocounts(0, gamma, y, y_depths)
         gamma = (t1 + y) / (t0 + t1 + y_depths)  # recalculate gamma
-    
-    # return alpha to be normalized to sum to 1 
-    return np.array([row/row.sum() for row in new_alpha]), gamma 
- 
 
- ########################  run em  ########################
-   
- 
-def em(x, x_depths, y, y_depths, num_iterations): 
+    # return alpha to be normalized to sum to 1
+    return np.array([row / row.sum() for row in new_alpha]), gamma
+
+
+########################  run em  ########################
+
+
+def em(x, x_depths, y, y_depths, num_iterations):
     """
-    run EM 
+    run EM
     x: the methylation counts for cfdna input
     x_depths: the total read depths for cfdna input
-    y: the number of methylation counts for the reference 
-    y_depths: the number of reads for the reference 
+    y: the number of methylation counts for the reference
+    y_depths: the number of reads for the reference
     num_iterations: number of iterations to complete
     """
-    
-    # randomly intialize alpha for each iteration 
+
+    # randomly intialize alpha for each iteration
     alpha = np.random.uniform(size=(x.shape[0], y.shape[0]))
-    alpha = np.array([row/row.sum() for row in alpha])  # make alpha sum to 1 
+    alpha = np.array([row / row.sum() for row in alpha])  # make alpha sum to 1
 
     # begin by checking for instances where there are no counts for y or y_depths
-    add_pseduocounts(1, np.nan_to_num(y/y_depths), y, y_depths)
-    add_pseduocounts(0, np.nan_to_num(y/y_depths), y, y_depths)
-    
-    # intialize gamma to reference values 
-    gamma = y/y_depths
+    add_pseduocounts(1, np.nan_to_num(y / y_depths), y, y_depths)
+    add_pseduocounts(0, np.nan_to_num(y / y_depths), y, y_depths)
 
-    # perform EM for a given number of iterations 
-    for i in range(num_iterations): 
+    # intialize gamma to reference values
+    gamma = y / y_depths
 
-        p0, p1 = expectation(gamma, alpha) 
+    # perform EM for a given number of iterations
+    for i in range(num_iterations):
+
+        p0, p1 = expectation(gamma, alpha)
         # ll = log_likelihood(p0, p1, x_depths, x, y_depths, y, gamma, alpha)
         a, g = maximization(p0, p1, x, x_depths, y, y_depths)
-        
+
         # check convergence of alpha and gamma
-        alpha_diff = np.mean(abs(a-alpha))/np.mean(abs(alpha))
-        gamma_diff = np.mean(abs(g-gamma))/np.mean(abs(gamma))
+        alpha_diff = np.mean(abs(a - alpha)) / np.mean(abs(alpha))
+        gamma_diff = np.mean(abs(g - gamma)) / np.mean(abs(gamma))
 
-        if alpha_diff + gamma_diff < 0.001:  # if convergence criteria, break 
+        if alpha_diff + gamma_diff < 0.001:  # if convergence criteria, break
             print("break")
-            break 
+            break
 
-        else:  # set current evaluation of alpha and gamma 
-            alpha = a 
-            gamma = g 
-    
+        else:  # set current evaluation of alpha and gamma
+            alpha = a
+            gamma = g
 
     ll = log_likelihood(p0, p1, x_depths, x, y_depths, y, gamma, alpha)
-    
+
     return alpha, gamma, ll
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
 
-    output_dir = sys.argv[1]  # output directory 
-    rep_num = int(sys.argv[2])  # what rep number this is (i ran this in parallel with all my reps running simulataneously)
+    output_dir = sys.argv[1]  # output directory
+    rep_num = int(
+        sys.argv[2]
+    )  # what rep number this is (i ran this in parallel with all my reps running simulataneously)
     i = int(sys.argv[3])  # number of tissues
     j = int(sys.argv[4])  # number of cpgs
-    n = int(sys.argv[5])  # number of individuals 
+    n = int(sys.argv[5])  # number of individuals
     depth = int(sys.argv[6])  # input depth
     gamma_depth = int(sys.argv[7])  # reference read depth
-    pkl_file = str(sys.argv[8])  # pickle file of numpy array tissue proportions 
+    pkl_file = str(sys.argv[8])  # pickle file of numpy array tissue proportions
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    
-    Y_depths, Y, X_depths, X, alpha_true, gamma_int = generate_em_replicate(i, j, n, depth, gamma_depth, pkl_file)
 
+    Y_depths, Y, X_depths, X, alpha_true, gamma_int = generate_em_replicate(
+        i, j, n, depth, gamma_depth, pkl_file
+    )
 
     random_restarts = []
     # perform for 10 random restarts
-    for i in range(10): 
+    for i in range(10):
         alpha, gamma, ll = em(X, X_depths, Y, Y_depths, 1000)
         random_restarts.append((ll, alpha, gamma))
 
@@ -332,24 +357,11 @@ if __name__ == "__main__":
     with open(output_dir + "/" + str(rep_num) + "_alpha_est.pkl", "wb") as f:
         pkl.dump(alpha_max, f)
 
-    with open(output_dir + "/" + str(rep_num)  + "_alpha_true.pkl", "wb") as f:
+    with open(output_dir + "/" + str(rep_num) + "_alpha_true.pkl", "wb") as f:
         pkl.dump(alpha_true, f)
 
     with open(output_dir + "/" + str(rep_num) + "_gamma_est.pkl", "wb") as f:
         pkl.dump(gamma_max, f)
 
-    with open(output_dir + "/" + str(rep_num)  + "_gamma_true.pkl", "wb") as f:
+    with open(output_dir + "/" + str(rep_num) + "_gamma_true.pkl", "wb") as f:
         pkl.dump(gamma_int, f)
-
-
-
-
-
-
-
-
-
-
-
-
-
--- TIMs/tim.py
+++ TIMs/tim.py
@@ -7,116 +7,138 @@ import sys
 
 
 def distance(values):
-	"""
-	calculates the absolute difference between the tissue methylation
-	and the median for a CpG
+    """
+    calculates the absolute difference between the tissue methylation
+    and the median for a CpG
 
-	values: array of methylation proportions for all tissues
+    values: array of methylation proportions for all tissues
 
-	return: distance between each tissue methylation prop and median, CpG
-	median methylation
-	"""
+    return: distance between each tissue methylation prop and median, CpG
+    median methylation
+    """
 
-	median = bn.nanmedian(values)
-	dist = np.abs(values - median)
-	return dist, median
+    median = bn.nanmedian(values)
+    dist = np.abs(values - median)
+    return dist, median
 
 
 def add_to_heap(heap, n, dist, median, cpg, percent, num):
-	"""
-	keep track of the top CpGs using a max heap of size n
+    """
+    keep track of the top CpGs using a max heap of size n
 
-	heap: max heap keeping track of top tims for a specific tissues
-	n: size of max heap
-	dist: distance between tissue methylation and median methylation
-	median: median methylation for CpG
-	cpg: cpg positional information (tuple of chrom, start, end)
-	percent: tissue methylation percent
-	num: tissue number
+    heap: max heap keeping track of top tims for a specific tissues
+    n: size of max heap
+    dist: distance between tissue methylation and median methylation
+    median: median methylation for CpG
+    cpg: cpg positional information (tuple of chrom, start, end)
+    percent: tissue methylation percent
+    num: tissue number
 
-	return: none
-	"""
+    return: none
+    """
 
-	if len(heap) < n:  # only keep track of the n CpGs with greatest distance
-		heapq.heappush(heap, (dist, cpg, median, percent, num))  # note- works because it only checks value of dist, not the rest of the tuple
-	else:
-		heapq.heappushpop(heap, (dist, cpg, median, percent, num))
+    if len(heap) < n:  # only keep track of the n CpGs with greatest distance
+        heapq.heappush(
+            heap, (dist, cpg, median, percent, num)
+        )  # note- works because it only checks value of dist, not the rest of the tuple
+    else:
+        heapq.heappushpop(heap, (dist, cpg, median, percent, num))
 
 
 def get_cpgs(heap_list):
-	"""
-	converts heap to a default dict in order to print
+    """
+    converts heap to a default dict in order to print
 
-	heap_list: list of max heaps- size is the number of tissues
+    heap_list: list of max heaps- size is the number of tissues
 
-	return: dictionary of CpGs and TIM values
-	"""
+    return: dictionary of CpGs and TIM values
+    """
 
-	cpgs = defaultdict(list)
+    cpgs = defaultdict(list)
 
-	for heap in heap_list:
-		for value in heap:  # iterate over list of heaps
+    for heap in heap_list:
+        for value in heap:  # iterate over list of heaps
 
-			# dictionary is CpG position info: number of tissue, distance between
-			# tissue meth and median, methylation percent for tissue, median CpG meth
-			cpgs[tuple(value[1])].append((value[4], value[0], value[3], value[2]))
+            # dictionary is CpG position info: number of tissue, distance between
+            # tissue meth and median, methylation percent for tissue, median CpG meth
+            cpgs[tuple(value[1])].append((value[4], value[0], value[3], value[2]))
 
-	return cpgs
+    return cpgs
 
 
 if __name__ == "__main__":
 
-	# user parameters
-	input_file = sys.argv[1] # input bedfile of WGBS data
-	output_file = sys.argv[2]  # path to output file
-	num_values = int(sys.argv[3])  # number of values to keep as tims
-	tissues = int(sys.argv[4]) # total number of tissues to calc tims for
-	depth_filter = int(sys.argv[5]) # depth filter
-	nan_filter = int(sys.argv[6])  # number of nans to allow in TIM calc
+    # user parameters
+    input_file = sys.argv[1]  # input bedfile of WGBS data
+    output_file = sys.argv[2]  # path to output file
+    num_values = int(sys.argv[3])  # number of values to keep as tims
+    tissues = int(sys.argv[4])  # total number of tissues to calc tims for
+    depth_filter = int(sys.argv[5])  # depth filter
+    nan_filter = int(sys.argv[6])  # number of nans to allow in TIM calc
 
-	# list of heaps needed to count max distances for each tissue
-	distance_heaps = [[] for i in range(tissues)]
+    # list of heaps needed to count max distances for each tissue
+    distance_heaps = [[] for i in range(tissues)]
 
-	with open(input_file, "r") as f:
-		bed = csv.reader(f, delimiter="\t")
+    with open(input_file, "r") as f:
+        bed = csv.reader(f, delimiter="\t")
 
-		for line in bed:
-			cpg = line[0:3]  # positional information
-			meth = np.asarray(line[3::2],  dtype="float")
-			depth = np.asarray(line[4::2], dtype="float")
+        for line in bed:
+            cpg = line[0:3]  # positional information
+            meth = np.asarray(line[3::2], dtype="float")
+            depth = np.asarray(line[4::2], dtype="float")
 
-			median_depth = bn.nanmedian(depth)
+            median_depth = bn.nanmedian(depth)
 
-			np.seterr(divide='ignore', invalid='ignore')  # ignore Nans when there are no counts
-			percents = meth/depth
+            np.seterr(
+                divide="ignore", invalid="ignore"
+            )  # ignore Nans when there are no counts
+            percents = meth / depth
 
-			nan_count = np.count_nonzero(np.isnan(percents))
+            nan_count = np.count_nonzero(np.isnan(percents))
 
-			if median_depth >= depth_filter and nan_count < nan_filter: # data must pass basic quality to be a tim
-				dist, median = distance(percents)
+            if (
+                median_depth >= depth_filter and nan_count < nan_filter
+            ):  # data must pass basic quality to be a tim
+                dist, median = distance(percents)
 
-				for i, col_dist in enumerate(dist):
-					if not np.isnan(col_dist):
-						add_to_heap(distance_heaps[i], num_values, col_dist, median, cpg, percents[i], i)
+                for i, col_dist in enumerate(dist):
+                    if not np.isnan(col_dist):
+                        add_to_heap(
+                            distance_heaps[i],
+                            num_values,
+                            col_dist,
+                            median,
+                            cpg,
+                            percents[i],
+                            i,
+                        )
 
+    cpgs = get_cpgs(distance_heaps)  # get TIM heap for printing
 
-	cpgs = get_cpgs(distance_heaps)  # get TIM heap for printing
+    # write the output file as tab delimited file with columns being:
+    # chrom, start, end, tissue # for tim, absolute difference, methylation prop for tissue, median methylation
+    # for all other tissues
+    with open(output_file, "w") as o:
+        dist_out = csv.writer(o, delimiter="\t")
 
+        # header
+        dist_out.writerow(
+            [
+                "chrom",
+                "start",
+                "end",
+                "tissue number",
+                "difference",
+                "tissue methylation",
+                "other tissue methylation",
+            ]
+        )
 
-	# write the output file as tab delimited file with columns being:
-	# chrom, start, end, tissue # for tim, absolute difference, methylation prop for tissue, median methylation
-	# for all other tissues
-	with open(output_file, "w") as o:
-			dist_out = csv.writer(o, delimiter="\t")
+        for c in cpgs:
 
-			# header 
-			dist_out.writerow(["chrom", "start", "end", "tissue number", "difference", "tissue methylation", "other tissue methylation"])
+            if len(cpgs[c]) == 1:  # exclude CpGs where it is a TIM for > 1 tissue
 
-			for c in cpgs:
+                position = [c[0], c[1], c[2]]
+                entry = [cpgs[c][0][0], cpgs[c][0][1], cpgs[c][0][2], cpgs[c][0][3]]
 
-				if len(cpgs[c]) == 1:  # exclude CpGs where it is a TIM for > 1 tissue
-
-					position = [c[0], c[1], c[2]]
-					entry = [cpgs[c][0][0], cpgs[c][0][1], cpgs[c][0][2], cpgs[c][0][3]]
-
-					dist_out.writerow(position + entry)
+                dist_out.writerow(position + entry)
